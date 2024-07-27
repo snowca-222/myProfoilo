f061b77eaab381b8ccdc5afc427edd9f291ea96a
snowca-222 <holybell222@gmail.com>
2024-07-28 02:22:49 +0800

    修正

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { SetStateAction, useState } from "react";
+import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import data from "./libs/js/user.json";
